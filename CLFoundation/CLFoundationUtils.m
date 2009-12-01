b83ec4f5ff7a51ba573f4e5d1f047f686783d77f
ettore <e@cubelogic.org>
2009-12-01 07:02:01 +0000

    added proper import
    git-svn-id: http://cubelogic.org/svn/clutils@15 b420a0ce-4a65-0410-9394-835c4d8ec028

--- CLFoundation/CLFoundationUtils.m
+++ CLFoundation/CLFoundationUtils.m
@@ -4,6 +4,8 @@
  *  Copyright 2009 Cubelogic. All rights reserved.
  */
 
+#import "CLFoundationUtils.h"
+
 Boolean isEmpty(NSString *s)
 {
     return (s == nil || [s compare:@""] == NSOrderedSame);
